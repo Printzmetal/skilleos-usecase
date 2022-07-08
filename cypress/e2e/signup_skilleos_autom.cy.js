d501d68bbbeb1055ec197c8ae719f363f42376b2
Patrick Chalard <pchalard@ensc.fr>
2022-07-08 23:01:21 +0200

    Premier commit use case skilleos cypress

---
+++ cypress/e2e/signup_skilleos_autom.cy.js
@@ -0,0 +1,9 @@
+describe('Use case QA Skilleos', () => {
+  it('Inscription sur le site de Skilleos', () => {
+
+      cy.visit('https://moncompte.skilleos.com/authentification#blocconnexion') // accès au site
+      
+  })
+
+
+})
